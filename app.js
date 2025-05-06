e45e0db6d7f07be47e3095009fb2dee11e0bb762
Shivam Sharma <shivamsharma3835@gmail.com>
2025-05-06 15:18:23 +0530

    test

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello this is deployed file comes from local system and hosted on aws with the help of git and jenkins')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  console.log(`Example app listening on port ${port}`)
   console.log(`Example app listening on port ${port}`)
 })
